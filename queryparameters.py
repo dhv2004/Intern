1ded22e3ff9b8a31d300ab42da32f08d0f57f28c
Dhruv Kaushal <dhruvkaushal69@gmail.com>
2025-07-22 22:48:12 +0530

    Using Queryparameters and Put function using FAST api

--- queryparameters.py
+++ queryparameters.py
@@ -3,7 +3,11 @@ from pydantic import BaseModel
 from typing import Optional
 
 app=FastAPI()
-
+class blog(BaseModel):
+    title : str
+    body : str 
+    published : Optional[bool]
+    
 @app.get('/')
 def query():
     return {'data' : {'this is query parameters'}}
@@ -15,3 +19,8 @@ def index(limit=10,published :bool = True , sort :Optional[str] = None):
         return {'data' : f'{limit} published blogs from db'}
     else :
         return{'data' : f'{limit} from db'}
+# How to access body and use of put function
+@app.put('/blog')
+def create_blog(request : blog):
+    
+    return {'data' : f"blog is created with title as {request.title}"}
