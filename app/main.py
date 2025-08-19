6a261d42513b03db0f8b199d11b9d3647df74ef8
dhv2004 <dhruvkaushal69@gmail.com>
2025-08-20 00:19:59 +0530

    Changes made with mobaXterm

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def get_db():
 @app.post('/Moba')
 def changes():
     return {"message" : "This was added using terminal of mobaXterm"
-     
+}     
 @app.post('/register')
 def register_user(user: schema.Userdata, db: Session = Depends(get_db)):
     try:
